4d74872776d84c4076c2121ddeedb8e73ef882de
Jani Purhonen <razorih@users.noreply.github.com>
2024-12-02 11:34:51 +0200

    Day 02 - Gold

--- src/bin/day02.rs
+++ src/bin/day02.rs
@@ -24,6 +24,65 @@ impl Levels {
     }
 }
 
+/// Check if given levels are valid.
+/// Returns invalid position if not
+fn check(levels: &[usize]) -> (bool, Option<usize>) {
+    // look at first two elements to determine direction
+    let dir = if levels[0] < levels[1] {
+        Levels::Increasing
+    } else {
+        Levels::Decreasing
+    };
+
+    for (i, w) in levels.windows(2).enumerate() {
+        if !dir.is_valid(w[0], w[1]) {
+            return (false, Some(i));
+        }
+    }
+
+    (true, None)
+}
+
+fn gold(input: &str) -> usize {
+    let mut safe: usize = 0;
+
+    for report in input.lines() {
+        let mut levels: Vec<usize> = report.split_ascii_whitespace()
+            .map_while(|num| num.parse::<usize>().ok())
+            .collect();
+
+        if let (true, _) = check(&levels) {
+            println!("safe without removal: {:?}", &levels);
+            safe += 1;
+            continue
+        }
+
+        if let (false, Some(invalid_i)) = check(&levels) {
+            // remove potentially bad levels and check again
+            let mut forwards = levels.clone();
+            let mut backwards = levels.clone();
+            levels.remove(invalid_i);
+            forwards.remove(invalid_i + 1); // index is always valid due to .windows(2) in check()
+            backwards.remove(invalid_i.saturating_sub(1));
+
+            if let (true, _) = check(&levels) {
+                println!("safe with removal: {:?} (removed index {})", &levels, invalid_i);
+                safe += 1;
+            } else if let (true, _) = check(&forwards) {
+                println!("safe with forward removal: {:?}", &forwards);
+                safe += 1;
+            } else if let (true, _) = check(&backwards) {
+                println!("safe with backwards removal: {:?}", &backwards);
+                safe += 1;
+            } else {
+                println!("truly unsafe: {:?}", &levels);
+            }
+        }
+    }
+
+    safe
+}
+
 fn silver(input: &str) -> usize {
     let mut safe: usize = 0;
 
@@ -32,15 +91,7 @@ fn silver(input: &str) -> usize {
             .map_while(|num| num.parse::<usize>().ok())
             .collect();
 
-        // look at first two elements to determine direction
-        let dir = if levels[0] < levels[1] {
-            Levels::Increasing
-        } else {
-            Levels::Decreasing
-        };
-
-        let is_safe = levels.windows(2).all(|win| dir.is_valid(win[0], win[1]));
-        if is_safe {
+        if let (true, _) = check(&levels) {
             safe += 1;
         }
     }
@@ -52,6 +103,7 @@ fn main() -> io::Result<()> {
     let input = read_input()?;
 
     println!("silver: {}", silver(&input));
+    println!("gold: {}", gold(&input));
 
     Ok(())
 }
