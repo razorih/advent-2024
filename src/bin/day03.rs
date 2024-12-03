83d10a5d773db43beac9611893a49c0a164de3b9
Jani Purhonen <razorih@users.noreply.github.com>
2024-12-03 14:04:59 +0200

    Day 03 - Gold

--- src/bin/day03.rs
+++ src/bin/day03.rs
@@ -2,58 +2,94 @@ use std::io;
 
 use advent::read_input;
 
-fn silver(input: &str) -> i64 {
+/// Try to parse a single `mul(xx,yy)` instruction.
+///
+/// Returns [`None`] if instruction is not valid
+fn try_parse_mul(input: &str) -> Option<i64> {
+    let mut num_buff = String::new();
+    let mut first: Option<i64> = None;
+
+    // skip "mul(" portion of the string
+    for chr in input[4..].chars() {
+        if chr.is_ascii_digit() {
+            num_buff.push(chr);
+            continue;
+        }
+
+        if chr == ',' {
+            if !num_buff.is_empty() {
+                first = num_buff.parse().ok();
+                num_buff.clear();
+            }
+            continue;
+        }
+
+        if chr == ')' {
+            if !num_buff.is_empty() {
+                let second: Option<i64> = num_buff.parse().ok();
+                num_buff.clear();
+
+                // mul() op is ready, return result if valid
+                if let (Some(a), Some(b)) = (first, second) {
+                    return Some(a * b);
+                }
+            }
+
+            // invalid operands
+            // either or both operands failed to parse
+            // or num_buff were empty (invalid digits)
+            return None
+        }
+
+        // println!("invalid mul(), got {}", chr);
+        return None
+    }
+
+    unreachable!()
+}
+
+fn gold(input: &str) -> i64 {
     let mut result = 0;
+    let mut enabled = true; // are mul() ops enabled?
 
-    'mul: for (mul_start, _) in input.match_indices("mul(") {
-        let mut num_buff = String::new();
-
-        // mul operands
-        let mut first: Option<i64> = None;
-        let mut second: Option<i64> = None;
-        
-        // skip "mul(" portion of the string
-        for chr in input[mul_start+4..].chars() {
-            if chr.is_ascii_digit() {
-                num_buff.push(chr);
-                continue;
-            }
-
-            if chr == ',' {
-                if !num_buff.is_empty() {
-                    first = num_buff.parse().ok();
-                    num_buff.clear();
+    for (i, chr) in input.char_indices() {
+        if chr == 'm' {
+            if let Some("mul(") = input.get(i..i+4) {
+                if enabled {
+                    result += try_parse_mul(&input[i..]).unwrap_or(0);
                 }
-                continue;
+            }
+        }
+
+        if chr == 'd' {
+            if let Some("do()") = input.get(i..i+4) {
+                enabled = true;
             }
 
-            if chr == ')' {
-                if !num_buff.is_empty() {
-                    second = num_buff.parse().ok();
-                    num_buff.clear();
-
-                    // multiply if we successfully parsed both numbers
-                    if let (Some(a), Some(b)) = (first, second) {
-                        result += a * b;
-                    }
-                }
-
-                // this mul() op is ready, move on
-                continue 'mul;
+            if let Some("don't()") = input.get(i..i+7) {
+                enabled = false;
             }
-
-            // println!("invalid mul(), got {}", chr);
-            continue 'mul;
         }
     }
 
     result
 } 
 
+fn silver(input: &str) -> i64 {
+    let mut result = 0;
+
+    for (mul_start, _) in input.match_indices("mul(") {
+        result += try_parse_mul(&input[mul_start..]).unwrap_or(0);
+    }
+
+    result
+}
+
 fn main() -> io::Result<()> {
     let input = read_input()?;
 
     println!("silver: {}", silver(&input));
+    println!("gold: {}", gold(&input));
 
     Ok(())
 }
